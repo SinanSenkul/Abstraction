54b982da85e51b3251b04fc5f238a706ff27f810
sinan <sinan@SINAN-SENKUL>
2024-08-14 05:14:22 +0300

    Add project files.

---
+++ Abstraction/BaseEmployee.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+public class EmployeeBase //defined the base class with standard values like name, lastname etc.
+{
+    public string Name { get; set; }
+    public string LastName { get; set; }
+    public string Department { get; set; }
+    public EmployeeBase(string name, string lastName, string department)
+    {
+        Name = name;
+        LastName = lastName;
+        Department = department;
+    }
+}
+public interface IObjective //defined the interface
+{
+    public abstract void Objective(); //Gorev() method has been defined as an abstract 
+}
+
+public class Developer : EmployeeBase, IObjective //developer class, which uses EmployeeBase as base class and IObjective as
+    //interface
+{
+    public string Position { get; set; }
+    public Developer(string name, string lastname, string department, string position) : base(name, lastname, department)
+    {
+        Name = name;
+        LastName = lastname;
+        Department = department;
+        Position = position;
+    }
+    public void Objective()
+    {
+        Console.WriteLine($"Merhaba! Adım {Name}, Şirkette developer olarak çalışıyorum");
+    }
+}
+
+public class SalesSpecialist : EmployeeBase, IObjective
+{
+    public string Position { get; set; }
+    public SalesSpecialist(string name, string lastname, string department, string position) : base(name, lastname, department)
+    {
+        Name = name;
+        LastName = lastname;
+        Department = department;
+        Position = position;
+    }
+    public void Objective()
+    {
+        Console.WriteLine($"Merhaba! Adım {Name}, Şirkette developer olarak çalışıyorum");
+    }
+}
---
+++ Abstraction/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Abstraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IObjective person = new Developer("sinan", "şenkul", "web development", "developer");
+            person.Objective();
+        }
+    }
+}
